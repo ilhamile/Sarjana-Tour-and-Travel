ce548138b84cd97dd6fbd5da8e13531f47f37445
ilhamile <75151743+ilhamile@users.noreply.github.com>
2021-01-22 16:06:20 +0700

    Add files via upload

---
+++ proses.php
@@ -0,0 +1,38 @@
+<?php 
+
+include 'class/user.php';
+
+
+$db= new user();
+
+
+
+
+$aksi = $_GET['aksi'];
+ if($aksi == "tambah"){
+ 	$db->input( $_FILES['foto']['name'],$_POST['nama_user'],$_POST['email_user'],$_POST['no_hp'],$_POST['no_rek'],$_POST['nama_rek'],$_POST['username'],$_POST['password'],$_POST['tgl_lahir'],$_POST['jekel'],$_POST['alamat']);
+ 	
+ 	header("location:index.php");
+
+ }elseif($aksi == "hapus"){ 	
+ 	$db->hapus($_GET['id']);
+	header("location:profil.php");
+
+}elseif($aksi == "update"){
+ 	$db->update($_POST['id_user'],$_FILES['foto']['name'],$_POST['nama_user'],$_POST['email_user'],$_POST['no_hp'],$_POST['no_rek'],$_POST['nama_rek'],$_POST['username'],$_POST['password'],$_POST['tgl_lahir'],$_POST['jekel'],$_POST['alamat']);
+ 	header("location:profil.php");
+ }
+
+ elseif($aksi == "tambah_order"){
+ 	if(!($db->check($_POST['id_paket'], $_POST['tgl_tour']))) {
+		echo "<script>alert('Kuota Penuh')</script>";
+ 		header("refresh:1;url=order.php");
+	} else {
+		$db->order($_POST['id_user'],$_POST['id_paket'],$_POST['id_daerah'],$_POST['tgl_pesan'],$_POST['tgl_tour']);
+ 		header("location:orderlist.php");
+	}
+ 	
+ 	
+ }
+
+?>
